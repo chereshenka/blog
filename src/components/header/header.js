e54a584d89599c11a0939d97ba05e68d4159b1fc
chereshenka <ladya.ferz@gmail.com>
2022-12-17 11:34:55 +0300

    header avatar, styles bug, tag drop app

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -17,57 +17,61 @@ const Header = () => {
 
   return (
     <>
-      <div className={styles.header}>
-        <Link
-          to="/"
-          className={styles.header_title}
-          onClick={() => dispatch(fetchArticlesGlobal())}
-        >
-          Realworld Blog
-        </Link>
-        <div className={styles.header_enterButtons}>
-          {isLoggedIn ? (
-            <>
-              <Link to="/new-article" className={styles.header_newarticle}>
-                Create article
-              </Link>
-              <div
-                onClick={() => push("/profile")}
-                className={styles.header_logged_user}
-              >
-                <p className={styles.header_logged_username}>{user.username}</p>
-                <img
-                  onError={() => setImageError(true)}
-                  src={imageError ? avatar : user.image}
-                  className={styles.header_logged_avatar}
-                />
-              </div>
-              <Link
-                to="/"
-                onClick={() => {
-                  dispatch(
-                    loginUser({
-                      isLoggedIn: false,
-                      user: {},
-                    }),
-                  );
-                  localStorage.removeItem("token");
-                }}
-                className={styles.header_logout}
-              >
-                Log Out
-              </Link>
-            </>
-          ) : (
-            <>
-              <Link to="/sing-in" className={styles.header_login}>
-                Sing In
-              </Link>
-              <Link to="/sing-up" className={styles.header_register}>
-                Sing Up
-              </Link>
-            </>
-          )}
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <Link
+            to="/"
+            className={styles.header_title}
+            onClick={() => dispatch(fetchArticlesGlobal())}
+          >
+            Realworld Blog
+          </Link>
+          <div className={styles.header_enterButtons}>
+            {isLoggedIn ? (
+              <>
+                <Link to="/new-article" className={styles.header_newarticle}>
+                  Create article
+                </Link>
+                <div
+                  onClick={() => push("/profile")}
+                  className={styles.header_logged_user}
+                >
+                  <p className={styles.header_logged_username}>
+                    {user.username}
+                  </p>
+                  <img
+                    onError={() => setImageError(true)}
+                    src={imageError ? avatar : user.image}
+                    className={styles.header_logged_avatar}
+                  />
+                </div>
+                <Link
+                  to="/"
+                  onClick={() => {
+                    dispatch(
+                      loginUser({
+                        isLoggedIn: false,
+                        user: {},
+                      }),
+                    );
+                    localStorage.removeItem("token");
+                  }}
+                  className={styles.header_logout}
+                >
+                  Log Out
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link to="/sing-in" className={styles.header_login}>
+                  Sing In
+                </Link>
+                <Link to="/sing-up" className={styles.header_register}>
+                  Sing Up
+                </Link>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </>
